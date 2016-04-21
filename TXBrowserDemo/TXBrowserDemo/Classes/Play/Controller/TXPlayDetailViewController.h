1911c3e1e22cd61e473df965cc3b9d9ffe54a508
李昕 <tingxins@lixins-iMac.local>
2016-04-21 15:18:40 +0800

    add Demo plist files

---
+++ TXBrowserDemo/TXBrowserDemo/Classes/Play/Controller/TXPlayDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  TXPlayDetailViewController.h
+//  TXBrowserDemo
+//
+//  Created by 李昕 on 4/21/16.
+//  Copyright © 2016 TX. All rights reserved.
+//
+
+#import "TXBasicSettingViewController.h"
+
+@interface TXPlayDetailViewController : TXBasicSettingViewController
+
+@end
--- TXBrowserDemo/TXBrowserDemo/Classes/Play/Controller/TXPlayViewController.m
+++ TXBrowserDemo/TXBrowserDemo/Classes/Play/Controller/TXPlayViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "TXPlayViewController.h"
+#import "TXPlayDetailViewController.h"
 
 #import "TXSystemMusicModel.h"
 #import "TXCommandModel.h"
@@ -36,8 +37,8 @@
     for (int index = 0; index < datasoucePlist.count; ++ index) {
         NSDictionary *dictionary = datasoucePlist[index];
         NSString *groupName = dictionary[@"groupName"];
-        if ([groupName isEqualToString:@"一人嗨"]) {
-            TXLog(@"一人嗨");
+        if ([groupName isEqualToString:@"第一分区"]) {
+            TXLog(@"第一分区");
             /** 1.第一个分组 **/
             NSMutableArray *itemArrows = [NSMutableArray array];
             for (NSDictionary *groupDict in dictionary[@"groupMembers"]) {
@@ -100,98 +101,101 @@
 
 - (void)setupDataSource {
     
-    /** 1.第一个分组 **/
-    NSString *demo0Name = @"生日祝福";
-    TXArrowImageItemModel *demo0 = [TXArrowImageItemModel arrowImageItemModelWithClassType:[TXArrowImageItemCell class] title:demo0Name imageName:@"egg" operation:^{
-        /** 点击父cell操作 **/
-        TXArrowItemModel *birthMusicItemModel_Chinese = [TXArrowItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"生日快乐歌（中文）" operation:^{
-            /** 点击子cell操作 **/
-            
-        }];
-        TXArrowItemModel *birthMusicItemModel_English = [TXArrowItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"生日快乐歌（英文）" operation:^{
-            /** 点击子cell操作 **/
-            
-        }];
-        
-        TXItemGroupModel *group0 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"" groupFooterTitle:@"" groupMembers:@[birthMusicItemModel_Chinese, birthMusicItemModel_English]];
-        [self pushDetailViewControllerWithTitle:demo0Name detailSources:[NSMutableArray arrayWithObject:group0]];
-    }];
-    demo0.customAccessoryImageName = @"arrow_right";
-    demo0.customAccessoryText = @"2";
-    
-    NSString *demo1Name = @"节日祝福";
-    TXArrowImageItemModel *demo1 = [TXArrowImageItemModel arrowImageItemModelWithClassType:[TXArrowImageItemCell class] title:demo1Name imageName:@"egg" operation:^{
-        /** 点击操作 **/
-        [self pushDetailViewControllerWithTitle:demo1Name detailSources:nil];
-    }];
-    demo1.customAccessoryImageName = @"arrow_right";
-    demo1.customAccessoryText = @"0";
-    
-    TXArrowImageItemModel *demo2 = [TXArrowImageItemModel arrowImageItemModelWithClassType:[TXArrowImageItemCell class] title:@"表白" imageName:@"egg" operation:^{
-        /** 点击操作 **/
-        [self pushDetailViewControllerWithTitle:demo1Name detailSources:nil];
-    }];
-    demo2.customAccessoryImageName = @"arrow_right";
-    demo2.customAccessoryText = @"0";
-    
-    TXItemGroupModel *group0 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"一个人嗨" groupFooterTitle:@"" groupMembers:@[demo0, demo1, demo2]];
-    
-    /** 2.第二个分组 **/
-    TXBasicItemModel *itemModel_Double0 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"小夫妻" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Double0");
-        
-    }];
-    TXBasicItemModel *itemModel_Double1 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"新闻联播" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Double1");
-        
-    }];
-    TXBasicItemModel *itemModel_Double2 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"闺中密友" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Double2");
-        
-    }];
-    TXBasicItemModel *itemModel_Double3 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"真是醉了" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Double3");
-        
-    }];
-    TXBasicCollectionItemModel *collectionItemModel_Double = [TXBasicCollectionItemModel basicCollectionItemModelWithClassType:[TXBasicCollectionItemCell class] title:@"" items:[NSMutableArray arrayWithObjects:itemModel_Double0, itemModel_Double1, itemModel_Double2, itemModel_Double3, nil] operation:nil];
-    collectionItemModel_Double.collectionCellH = TXBasicCollectionViewWH;
-    TXItemGroupModel *group1 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"成双成对" groupFooterTitle:@"" groupMembers:@[collectionItemModel_Double]];
-    
-    /** 第三个分组 **/
-    TXBasicItemModel *itemModel_Triple0 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"西游记" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Triple0");
-        
-    }];
-    TXBasicItemModel *itemModel_Triple1 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"刘张关" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Triple1");
-        
-    }];
-    TXBasicItemModel *itemModel_Triple2 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"三个火枪手" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Triple2");
-        
-    }];
-    TXBasicItemModel *itemModel_Triple3 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"真是醉了" operation:^{
-        /** 点击子cell操作 **/
-        TXLog(@"itemModel_Triple3");
-        
-    }];
-    TXBasicCollectionItemModel *collectionItemModel_Triple = [TXBasicCollectionItemModel basicCollectionItemModelWithClassType:[TXBasicCollectionItemCell class] title:@"" items:[NSMutableArray arrayWithObjects:itemModel_Triple0, itemModel_Triple1, itemModel_Triple2, itemModel_Triple3, nil] operation:nil];
-    collectionItemModel_Double.collectionCellH = 150;
-    TXItemGroupModel *group2 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"三妻四妾" groupFooterTitle:@"" groupMembers:@[collectionItemModel_Triple]];
-    [self.dataSource addObject:group0];
-    [self.dataSource addObject:group1];
-    [self.dataSource addObject:group2];
+//    /** 1.第一个分组 **/
+//    NSString *demo0Name = @"生日祝福";
+//    TXArrowImageItemModel *demo0 = [TXArrowImageItemModel arrowImageItemModelWithClassType:[TXArrowImageItemCell class] title:demo0Name imageName:@"egg" operation:^{
+//        /** 点击父cell操作 **/
+//        TXArrowItemModel *birthMusicItemModel_Chinese = [TXArrowItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"生日快乐歌（中文）" operation:^{
+//            /** 点击子cell操作 **/
+//            
+//        }];
+//        TXArrowItemModel *birthMusicItemModel_English = [TXArrowItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"生日快乐歌（英文）" operation:^{
+//            /** 点击子cell操作 **/
+//            
+//        }];
+//        
+//        TXItemGroupModel *group0 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"" groupFooterTitle:@"" groupMembers:@[birthMusicItemModel_Chinese, birthMusicItemModel_English]];
+//        [self pushDetailViewControllerWithTitle:demo0Name detailSources:[NSMutableArray arrayWithObject:group0]];
+//    }];
+//    demo0.customAccessoryImageName = @"arrow_right";
+//    demo0.customAccessoryText = @"2";
+//    
+//    NSString *demo1Name = @"节日祝福";
+//    TXArrowImageItemModel *demo1 = [TXArrowImageItemModel arrowImageItemModelWithClassType:[TXArrowImageItemCell class] title:demo1Name imageName:@"egg" operation:^{
+//        /** 点击操作 **/
+//        [self pushDetailViewControllerWithTitle:demo1Name detailSources:nil];
+//    }];
+//    demo1.customAccessoryImageName = @"arrow_right";
+//    demo1.customAccessoryText = @"0";
+//    
+//    TXArrowImageItemModel *demo2 = [TXArrowImageItemModel arrowImageItemModelWithClassType:[TXArrowImageItemCell class] title:@"表白" imageName:@"egg" operation:^{
+//        /** 点击操作 **/
+//        [self pushDetailViewControllerWithTitle:demo1Name detailSources:nil];
+//    }];
+//    demo2.customAccessoryImageName = @"arrow_right";
+//    demo2.customAccessoryText = @"0";
+//    
+//    TXItemGroupModel *group0 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"一个人嗨" groupFooterTitle:@"" groupMembers:@[demo0, demo1, demo2]];
+//    
+//    /** 2.第二个分组 **/
+//    TXBasicItemModel *itemModel_Double0 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"小夫妻" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Double0");
+//        
+//    }];
+//    TXBasicItemModel *itemModel_Double1 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"新闻联播" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Double1");
+//        
+//    }];
+//    TXBasicItemModel *itemModel_Double2 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"闺中密友" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Double2");
+//        
+//    }];
+//    TXBasicItemModel *itemModel_Double3 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"真是醉了" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Double3");
+//        
+//    }];
+//    TXBasicCollectionItemModel *collectionItemModel_Double = [TXBasicCollectionItemModel basicCollectionItemModelWithClassType:[TXBasicCollectionItemCell class] title:@"" items:[NSMutableArray arrayWithObjects:itemModel_Double0, itemModel_Double1, itemModel_Double2, itemModel_Double3, nil] operation:nil];
+//    collectionItemModel_Double.collectionCellH = TXBasicCollectionViewWH;
+//    TXItemGroupModel *group1 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"第二分区" groupFooterTitle:@"" groupMembers:@[collectionItemModel_Double]];
+//    
+//    /** 第三个分组 **/
+//    TXBasicItemModel *itemModel_Triple0 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"西游记" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Triple0");
+//        
+//    }];
+//    TXBasicItemModel *itemModel_Triple1 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"刘张关" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Triple1");
+//        
+//    }];
+//    TXBasicItemModel *itemModel_Triple2 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"三个火枪手" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Triple2");
+//        
+//    }];
+//    TXBasicItemModel *itemModel_Triple3 = [TXBasicItemModel basicItemModelWithClassType:[TXArrowItemCell class] title:@"真是醉了" operation:^{
+//        /** 点击子cell操作 **/
+//        TXLog(@"itemModel_Triple3");
+//        
+//    }];
+//    TXBasicCollectionItemModel *collectionItemModel_Triple = [TXBasicCollectionItemModel basicCollectionItemModelWithClassType:[TXBasicCollectionItemCell class] title:@"" items:[NSMutableArray arrayWithObjects:itemModel_Triple0, itemModel_Triple1, itemModel_Triple2, itemModel_Triple3, nil] operation:nil];
+//    collectionItemModel_Double.collectionCellH = 150;
+//    TXItemGroupModel *group2 = [TXItemGroupModel itemGroupModelWithGroupHeaderTitle:@"第三分区" groupFooterTitle:@"" groupMembers:@[collectionItemModel_Triple]];
+//    [self.dataSource addObject:group0];
+//    [self.dataSource addObject:group1];
+//    [self.dataSource addObject:group2];
 }
 
 - (void)pushDetailViewControllerWithTitle:(NSString *)title detailSources:(NSMutableArray *)detailSources {
-    
+    TXPlayDetailViewController *detailViewController = [[TXPlayDetailViewController alloc] init];
+    detailViewController.title = title;
+    detailViewController.dataSource = detailSources;
+    [self.navigationController pushViewController:detailViewController animated:YES];
 }
 
 - (void)setupSomeSubviews {
